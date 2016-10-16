02b4bc2fec266cefd17b3590ea7c6344249b6d11
Kevin Weng <kweng@mit.edu>
2016-10-16 01:07:19 -0400

    updated event and station data

--- event_std_dt.py
+++ event_std_dt.py
@@ -1,38 +1,44 @@
-import matplotlib.pyplot as plt 
 import glob
 import datetime
 import numpy as np
 from argparse import ArgumentParser
 
-parser = ArgumentParser()
+def main():
+    parser = createParser()
+    options = parser.parse_args()
 
-parser.add_argument('-e','-ext', 
-        dest='ext', help='file extension',
-        metavar='EXT', required=True)
+    now = datetime.datetime.now()
+    output_filename = options.ext + ".event.stdmean.of.stddelay." + now.strftime("%Y%m%d") + ".txt"
+    col_names = "filename, mean of std, std of delay_times, mean of delay_times\n"
 
-options = parser.parse_args()
+    with open(output_filename, 'w') as f:
+        f.write("Created on " + now.strftime("%b %d, %Y %H:%M") + '\n')
+        f.write(col_names)
 
-now = datetime.datetime.now()
-output_filename = options.ext + ".event.stdmean.of.stddelay." + now.strftime("%Y%m%d") + ".txt"
-col_names = "filename, mean of std, std of delay_times, mean of delay_times\n"
+        for file in glob.glob("*." + options.ext):
+            std_list = []
+            delay_times_list = []
+            output_filename = file
+            for line in open(file):
+                line_list = line.split()
+                if len(line_list) == 9:
+                    std_list.append(float(line_list[2]))
+                    delay_times_list.append(float(line_list[8]))
 
-with open(output_filename, 'w') as f:
-    f.write("Created on " + now.strftime("%b %d, %Y %H:%M") + '\n')
-    f.write(col_names)
+            output_line = output_filename  + '\t'
+            output_line += '{:.4f}'.format(np.mean(std_list)) + '\t'
+            output_line += '{:.4f}'.format(np.std(delay_times_list)) + '\t' + '{:.4f}'.format(np.mean(delay_times_list)) +'\n'
+            f.write(output_line)
 
-    for file in glob.glob("*." + options.ext):
-        std_list = []
-        delay_times_list = []
-        output_filename = file
-        for line in open(file):
-            line_list = line.split()
-            if len(line_list) == 9:
-                std_list.append(float(line_list[2]))
-                delay_times_list.append(float(line_list[8]))
+def createParser():
+    # Add cmd line argument for choosing directory
+    parser = ArgumentParser()
 
-        output_line = output_filename  + '\t'
-        output_line += '{:.4f}'.format(np.mean(std_list)) + '\t'
-        output_line += '{:.4f}'.format(np.std(delay_times_list)) + '\t' + '{:.4f}'.format(np.mean(delay_times_list)) +'\n'
-        f.write(output_line)
+    parser.add_argument('-e','-ext', 
+            dest='ext', help='file extension',
+            metavar='EXT', required=True)
 
-print ("Done!")
+    return parser
+
+if __name__ == '__main__':
+    main()
--- station_std_dt.py
+++ station_std_dt.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt 
 import glob
 import os
 import errno
@@ -6,60 +5,67 @@ import numpy as np
 import datetime
 from argparse import ArgumentParser
 
-stations = {}
-now = datetime.datetime.now()
+def main():
+    parser = createParser()
+    options = parser.parse_args()
 
-parser = ArgumentParser()
+    stations = {}
+    now = datetime.datetime.now()
 
-parser.add_argument('-e','-ext', 
-        dest='ext', help='file extension',
-        metavar='EXT', required=True)
+    for file in glob.glob("*." + options.ext):
+        for line in open(file):
+            line_list = line.split()
+            if len(line_list) == 9:
+                station_name  = line_list[0].replace('\x00', '')
+                std = float(line_list[2])
+                delay_time = float(line_list[8])
+                if station_name not in stations.keys():
+                    stations[station_name] = [[file], [std], [delay_time]]
+                else: 
+                    stations[station_name][0].append(file)
+                    stations[station_name][1].append(std)
+                    stations[station_name][2].append(delay_time)
 
-options = parser.parse_args()
+    for station in stations.keys():
+        output_filename = options.ext + "_stations_data/" + station + ".stdmean.of.stddelay." + now.strftime("%Y%m%d") + ".txt"
+        if not os.path.exists(os.path.dirname(output_filename)):
+            try:
+                os.mkdir(os.path.dirname(output_filename))
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
 
-for file in glob.glob("*." + options.ext):
-    for line in open(file):
-        line_list = line.split()
-        if len(line_list) == 9:
-            station_name  = line_list[0].replace('\x00', '')
-            std = float(line_list[2])
-            delay_time = float(line_list[8])
-            if station_name not in stations.keys():
-                stations[station_name] = [[file], [std], [delay_time]]
-            else: 
-                stations[station_name][0].append(file)
-                stations[station_name][1].append(std)
-                stations[station_name][2].append(delay_time)
+        col_names = "filename,\tstd,\tdelay_times\n"
+        with open(output_filename, 'w') as f:
+            f.write("Created on " + now.strftime("%b %d, %Y %H:%M") + '\n')
+            f.write(col_names)
 
-for station in stations.keys():
-    output_filename = options.ext + "_stations_data/" + station + ".stdmean.of.stddelay." + now.strftime("%Y%m%d") + ".txt"
-    if not os.path.exists(os.path.dirname(output_filename)):
-        try:
-            os.mkdir(os.path.dirname(output_filename))
-        except OSError as exc: # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
+            data = stations[station]
+            file = data[0]
+            stds = data[1]
+            delay_times = data[2]
+            for i in range(len(stds)):
+                output_line = file[i] + '\t' + str(stds[i]) + '\t' + str(delay_times[i]) + '\n'
+                f.write(output_line)
+            stdSTD = np.std(stds)
+            meanSTD = np.mean(stds)
+            stdDelay = np.std(delay_times)
+            meanDelay = np.mean(delay_times)
 
-    col_names = "filename,\tstd,\tdelay_times\n"
-    with open(output_filename, 'w') as f:
-        f.write("Created on " + now.strftime("%b %d, %Y %H:%M") + '\n')
-        f.write(col_names)
+            f.write("STD_of_std: " + '{:.4f}'.format(stdSTD) + '\n')
+            f.write("Mean_std: " + '{:.4f}'.format(meanSTD) + '\n')
+            f.write("STD_of_delay_times: " + '{:.4f}'.format(stdDelay) + '\n')
+            f.write("Mean_delay_times: " + '{:.4f}'.format(meanDelay) + '\n')
 
-        data = stations[station]
-        file = data[0]
-        stds = data[1]
-        delay_times = data[2]
-        for i in range(len(stds)):
-            output_line = file[i] + '\t' + str(stds[i]) + '\t' + str(delay_times[i]) + '\n'
-            f.write(output_line)
-        stdSTD = np.std(stds)
-        meanSTD = np.mean(stds)
-        stdDelay = np.std(delay_times)
-        meanDelay = np.mean(delay_times)
+def createParser():
+    # Add cmd line argument for choosing directory
+    parser = ArgumentParser()
 
-        f.write("STD_of_std: " + '{:.4f}'.format(stdSTD) + '\n')
-        f.write("Mean_std: " + '{:.4f}'.format(meanSTD) + '\n')
-        f.write("STD_of_delay_times: " + '{:.4f}'.format(stdDelay) + '\n')
-        f.write("Mean_delay_times: " + '{:.4f}'.format(meanDelay) + '\n')
+    parser.add_argument('-e','-ext', 
+            dest='ext', help='file extension',
+            metavar='EXT', required=True)
 
-print ("Done!")
+    return parser
+
+if __name__ == '__main__':
+    main()
